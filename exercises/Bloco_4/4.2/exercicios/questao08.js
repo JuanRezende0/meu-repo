5d156960d2287c67ea480811723f19f1c3977aa9
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-04-23 16:40:56 -0300

    resolucao da questao08 da aba de exercicios

---
+++ exercises/Bloco_4/4.2/exercicios/questao08.js
@@ -0,0 +1,11 @@
+// Questao 08 - Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let array = [];
+
+for(let aux = 1; aux < 26; aux++){
+
+    array.push(aux);
+
+}
+
+console.log(array);
