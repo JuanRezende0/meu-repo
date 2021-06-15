82f46cd243f27726dc865731b7e4aba11c7a6cc5
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-06-15 16:18:27 -0300

    pratique tests assincronos, callbacks, promises, async/await

--- Modulo_Fundamentos/bloco_10/10.2/pratique.test.js
+++ Modulo_Fundamentos/bloco_10/10.2/pratique.test.js
@@ -1,6 +1,110 @@
-test("Não deveria passar!", () => {
-  setTimeout(() => {
-    expect(10).toBe(5);
-    console.log('Deveria falhar!')
-  }, 500);
+// CALLBACKS
+
+// 1 - versao, ERRADO, o jest nao espera o teste acabar
+// test("Não deveria passar!", () => {
+//   setTimeout(() => {
+//     expect(10).toBe(5);
+//     console.log('Deveria falhar!')
+//   }, 500);
+// });
+
+// 2 - Agora o erro e identificado
+// test("Não deveria passar!", done => {
+//   setTimeout(() => {
+//     expect(10).toBe(5);
+//     console.log('Deveria falhar!')
+//     done();
+//   }, 500);
+// });
+
+// 3 - correto
+// const SumNumbers = (a, b, callback) => {
+//   setTimeout(() => {
+//     const result = a + b;
+//     callback(result);
+//   }, 500)
+// }
+// test('Testando SumNumbers, soma 5 mais 10', done => {
+//   SumNumbers(5, 10, (result) => {
+//     expect(result).toBe(15);
+//     done();
+//   });
+// })
+
+// PROMISES
+
+// 1
+
+// const Animals = [
+//   { name: 'Dorminhoco', age: 1, type: 'Dog' },
+//   { name: 'Soneca', age: 2, type: 'Dog' },
+//   { name: 'Preguiça', age: 5, type: 'Cat' },
+// ];
+
+// const findAnimalsByType = (type) => (
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const arrayAnimals = Animals.filter((animal) => animal.type === type);
+//       if (arrayAnimals.length !== 0) {
+//         return resolve(arrayAnimals);
+//       }
+
+//       return reject({ error: 'Não possui esse tipo de animal.' });
+//     }, 100);
+//   })
+// );
+
+// const getListAnimals = (type) => (
+//   findAnimalsByType(type).then(list => list)
+// );
+
+// describe('Quando o tipo do animal existe', () => {
+//   test('Retorne a lista de animais', () => {
+//     expect.assertions(2);
+//     return getListAnimals('Dog').then(listDogs => {
+//       expect(listDogs[0].name).toEqual('Dorminhoco');
+//       expect(listDogs[1].name).toEqual('Soneca');
+//     });
+//   });
+// });
+
+// ASYNC / AWAIT
+
+// 1
+
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalsByType = (type) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const arrayAnimals = Animals.filter((animal) => animal.type === type);
+      if (arrayAnimals.length !== 0) {
+        return resolve(arrayAnimals);
+      }
+
+      return reject({ error: 'Não possui esse tipo de animal.' });
+    }, 100);
+  })
+);
+
+const getListAnimals = (type) => (
+  findAnimalsByType(type).then(list => list)
+);
+
+test('Testando com async/await', async () => {
+  const listDogs = await getListAnimals('Dog');
+  expect(listDogs[0].name).toEqual('Dorminhoco');
+  expect(listDogs[1].name).toEqual('Soneca');
+});
+
+test('Testando com async/await, testando o reject', async () => {
+  try {
+    await getListAnimals('Lion');
+  } catch (error) {
+    expect(error).toEqual({ error: "Não possui esse tipo de animal." })
+  }
 });
